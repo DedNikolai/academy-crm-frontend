113d90f1cec3f9ab2eeebcbc0d511285a6e67cbc
DedNikolai <nikolai.blashchuk@gmail.com>
2024-11-03 22:20:43 +0200

    first commit

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import DashboardLayoutBasic from "../components/DashboardLayout";
+
+const Dashboard: FC = () => {
+    return (
+        <DashboardLayoutBasic />
+    )
+}
+
+export default Dashboard;
